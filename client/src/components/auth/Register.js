1e143b0824cdc1e47a5366b79f3667dc2bed6ca6
Luc Audemard <bjluc@bjluc.co.uk>
2020-03-17 13:49:32 +0000

    Register, login and logout added to navBar

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,11 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -22,9 +23,14 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('SUCCESS');
+      register({ name, email, password });
     }
   };
+
+  // Redirect if logged in
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
 
   return (
     <Fragment>
@@ -38,7 +44,6 @@ const Register = ({ setAlert }) => {
             type='text'
             placeholder='Name'
             name='name'
-            required
             value={name}
             onChange={e => onChange(e)}
           />
@@ -48,7 +53,6 @@ const Register = ({ setAlert }) => {
             type='email'
             placeholder='Email Address'
             name='email'
-            required
             value={email}
             onChange={e => onChange(e)}
           />
@@ -83,9 +87,16 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
-export default connect(null, {
-  setAlert
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, {
+  setAlert,
+  register
 })(Register);
